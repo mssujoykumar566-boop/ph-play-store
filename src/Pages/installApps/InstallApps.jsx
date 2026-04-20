3cbfc0e3c35b2244e7ad75a697ab2670ffe5ae7a
mssujoykumar566-boop <mssujoykumar566@gmail.com>
2026-04-20 09:14:23 +0600

    first-commit

--- src/Pages/installApps/InstallApps.jsx
+++ src/Pages/installApps/InstallApps.jsx
@@ -1,19 +1,35 @@
 import React, { useContext } from 'react';
 import { InstallAppsContext } from '../../context/InstallAppsContext';
-
+import { toast } from 'react-toastify';
 
 const InstallApps = () => {
 
     const {installedApp,setInstalledApp} = useContext(InstallAppsContext)
 
     console.log(installedApp,"contextData")
+
+    const handleUnistall = (app) => {
+        console.log(app,"need to unistall");
+
+        const restApps = installedApp.filter(iApp =>iApp.id != app.id)
+        console.log(restApps,'restApps');
+
+        setInstalledApp(restApps);
+          toast.warning(`${app.title} is uninstalled!`)
+    };
+
     return  (<div className='container mx-auto my-10'>
+        <h3>hello world</h3>
             {
-                installedApp.map((app,ind) => {
-                    return (<div className='flex gap-4 items-center shadow p-4 rounded-md bg-slate-100 mb-5' key={ind}>
+              installedApp.length === 0 ? <h2 className='font-bold text-4xl text-center my-5 bg-purple-500'>No installed app found!</h2> :  installedApp.map((app,ind) => {
+                    return (<div className='flex gap-4 items-center justify-between shadow p-4 rounded-md bg-slate-100 mb-5' key={ind}>
+                       <div>
+                     
                         <img className='h-30 w-auto' src={app.image} alt="" />
                         <h2 className='font-semibold terxt-2xl'>{app.title}</h2>
-                    </div>)
+                       </div>
+                        <button onClick={() => handleUnistall(app)} className='btn bg-secondary'>Uninstall</button>
+                    </div>);
                 })
             }
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router/dom";
-import "./index.css"
+import "./index.css";
 import { router } from "./router/Routes";
 import InstalledAppsProvider from "./context/InstalledAppsProvider";
 
